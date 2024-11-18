f9f26b0a8ab37d3f9a751af16f0acafad3c7ffcc
QuangLinh <nguyenlinhak4@gmail.com>
2024-11-18 23:10:58 +0700

    Linh commit nhánh quy lên Github

---
+++ BACKEND/smartdeals_service/src/main/java/data/smartdeals_service/repository/BlogImageRepository.java
@@ -0,0 +1,9 @@
+package data.smartdeals_service.repository;
+
+
+import data.smartdeals_service.models.BlogImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface BlogImageRepository extends JpaRepository<BlogImage, Long> {
+}
---
+++ BACKEND/smartdeals_service/src/test/java/data/smartdeals_service/SmartdealsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package data.smartdeals_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartdealsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
