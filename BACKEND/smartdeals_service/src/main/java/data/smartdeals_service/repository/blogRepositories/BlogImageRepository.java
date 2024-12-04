151167d1806c25ee40ff6cf26012813c8a79b8c3
QuangLinh <nguyenlinhak4@gmail.com>
2024-12-04 20:49:40 +0700

    Save untracked files before pulling changes

---
+++ BACKEND/smartdeals_service/src/main/java/data/smartdeals_service/repository/blogRepositories/BlogImageRepository.java
@@ -0,0 +1,9 @@
+package data.smartdeals_service.repository.blogRepositories;
+
+
+import data.smartdeals_service.models.blog.BlogImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface BlogImageRepository extends JpaRepository<BlogImage, Long> {
+}
