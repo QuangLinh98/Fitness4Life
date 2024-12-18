8018435124a84c00b21e4e8fc040ce999480d3d8
AnhQuy-Dev <huytnqh@gmail.com>
2024-12-18 23:05:39 +0700

    up4

--- src/main/java/data/smartdeals_service/dto/promotion/PromotionStatusDTO.java
+++
@@ -1,8 +0,0 @@
-package data.smartdeals_service.dto.promotion;
-
-import lombok.Data;
-
-@Data
-public class PromotionStatusDTO {
-    private Boolean isActive;
-}
--- src/main/java/data/smartdeals_service/models/promotion/CustomerType.java
+++
@@ -1,7 +0,0 @@
-package data.smartdeals_service.models.promotion;
-
-public enum CustomerType {
-    NEW,          // Khách hàng mới
-    RETURNING,    // Khách hàng quay lại
-    ALL           // Tất cả khách hàng
-}
