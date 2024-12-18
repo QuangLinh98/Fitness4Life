71a18d6b859894698db7486d2e197daec653a2b6
QuangLinh <nguyenlinhak4@gmail.com>
2024-12-18 22:11:40 +0700

    Xóa smartdeals-service

--- BACKEND/smartdeals-service/src/main/java/data/smartdeals_service/repository/forumRepositories/QuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package data.smartdeals_service.repository.forumRepositories;
-
-import data.smartdeals_service.models.forum.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuestionRepository extends JpaRepository<Question, Long> {
-}
