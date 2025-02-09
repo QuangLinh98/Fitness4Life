f19e3b63674f65e505f38cfa206bb79cfa535d42
QuangLinh <nguyenlinhak4@gmail.com>
2025-02-09 18:34:43 +0700

    thêm task getOneQuestionByI trong service

--- BACKEND/smartdeals-service/src/main/java/data/smartdeals_service/helpers/GlobalConstant.java
+++ BACKEND/smartdeals-service/src/main/java/data/smartdeals_service/helpers/GlobalConstant.java
@@ -1,6 +1,6 @@
 package data.smartdeals_service.helpers;
 
 public class GlobalConstant {
-    public static String rootUrl = "http://localhost:8082/";
+    public static String rootUrl = "http://localhost:8088/";
     public static String uploadFolder = "uploads";
 }
