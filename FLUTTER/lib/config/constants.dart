48b9c59c61c8cfab7e229d642767d68a59bd904a
minhle0303 <minhle03032000@gmail.com>
2025-02-24 13:53:54 +0700

    remove cached

--- FLUTTER/lib/config/constants.dart
+++
@@ -1,7 +0,0 @@
-
-const String SERVER_IP = "192.168.1.38"; // Đổi IP dễ dàng khi cần
-
-/// Hàm thay thế "localhost" bằng SERVER_IP
-String getFullImageUrl(String url) {
-  return url.replaceFirst("localhost", SERVER_IP);
-}
